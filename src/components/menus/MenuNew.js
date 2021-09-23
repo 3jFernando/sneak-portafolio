9dbb1abf938db75216dc0c2ee80511cfee6d3de5
styvensoft <steveen.echeverri@gmail.com>
2021-09-23 09:16:23 -0500

    Navbar creation with react-boostrap

--- src/components/menus/MenuNew.js
+++ src/components/menus/MenuNew.js
@@ -1,55 +1,40 @@
 import React from "react";
+import { 
+  Navbar,
+  Container,
+  Nav
+} from 'react-bootstrap';
 
 const LOGO = "/logo192.png";
 
 function MenuNew() {
   return (
-    <header id="header" className="header header-scrolled">
-      <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
-        <a href="index.html" className="text-decoration-none logo d-flex align-items-center">
-          <img src={LOGO} alt />
-          <span>
-            CLAROS SOLUCIONES
-          </span>
-        </a>
-        <nav id="navbar" className="navbar">
-          <ul>
-            <li>
-              <a className="nav-link scrollto active" href="#bussines">
-                Valores
-              </a>
-            </li>
-            <li>
-              <a className="nav-link scrollto" href="#contact">
-                Contacto
-              </a>
-            </li>
-            <li>
-              <a className="nav-link scrollto" href="#portfolio">
-                Portafolio
-              </a>
-            </li>
-            <li>
-              <a className="nav-link scrollto" href="#services">
-                servicios
-              </a>
-            </li>
-            <li>
-              <a className="nav-link scrollto" href="#clients">
-                Clientes
-              </a>
-            </li>
-            <li>
-              <a className="text-decoration-none getstarted scrollto" href="#about">
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          
+          <span>CLAROS SOLUCIONES</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+          <Nav className="justify-content-md-center">
+            <Nav.Link href="#bussines">Valores</Nav.Link>
+            <Nav.Link href="#contact">Contacto</Nav.Link>
+            <Nav.Link href="#portfolio">Portafolio</Nav.Link>
+            <Nav.Link href="#services">Servicios</Nav.Link>
+            <Nav.Link href="#clients">Clientes</Nav.Link>
+            <Nav.Link href="#features">
+              <a
+                className="text-decoration-none getstarted scrollto"
+                href="#about"
+              >
                 Cotizar
               </a>
-            </li>
-          </ul>
-          <i className="bi mobile-nav-toggle bi-list" />
-        </nav>
-        {/* .navbar */}
-      </div>
-    </header>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
