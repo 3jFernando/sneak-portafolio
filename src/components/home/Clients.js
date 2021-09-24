9d6c87f964589eff701d22431f6d522e89567fbc
styvensoft <steveen.echeverri@gmail.com>
2021-09-24 10:54:59 -0500

    Creation clients components with Swiper library

--- src/components/home/Clients.js
+++ src/components/home/Clients.js
@@ -1,125 +1,62 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Autoplay } from "swiper";
+import "swiper/swiper-bundle.css";
 
+//api
+const ApiClients = require('../../utils/CLIENTS.json');
+
+SwiperCore.use([Navigation, Autoplay]);
 
 function Clients(props) {
+
+  // const [items, setItems] = useState(ApiClients); // JSON -> items
+
   return (
     <div id="clients" className="Clients body-space">
       <h2>NUESTROS CLIENTES</h2>
       <br />
-      <div className="testimonial-item col-lg-3 col-md-6 d-flex align-items-stretch">
-        <div className="stars">
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-        </div>
-        <div className="profile">
-          <img
-            src="assets/clients/sualternativasas.png"
-            className="testimonial-img"
-            alt
-          />
-          <h3>SUMINISTROS Y SERVICIOS</h3>
-          <h4>ALTERNATIVOS S.A.S</h4>
-          <p>
-            Quis quorum aliqua sint quem legam fore sunt eram irure aliqua
-            veniam tempor noster veniam enim culpa labore duis sunt culpa nulla
-            illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-          </p>
-        </div>
-      </div>
-      <div className="testimonial-item col-lg-3 col-md-6 d-flex align-items-stretch">
-        <div className="stars">
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-        </div>
-        <div className="profile">
-          <img
-            src="assets/clients/naranjaerp.png"
-            className="testimonial-img"
-            alt
-          />
-          <h3>NaranjaERP S.A.S.</h3>
-
-          <p>
-            Quis quorum aliqua sint quem legam fore sunt eram irure aliqua
-            veniam tempor noster veniam enim culpa labore duis sunt culpa nulla
-            illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-          </p>
-        </div>
-      </div>
-      <div className="testimonial-item col-lg-3 col-md-6 d-flex align-items-stretch">
-        <div className="stars">
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-        </div>
-        <div className="profile">
-          <img
-            src="assets/clients/mcs.png"
-            className="testimonial-img"
-            alt
-          />
-          <h3>MCS Consultoria</h3>
-          <h4>Facturación Electronica <strong>DIAN</strong></h4>
-          <p>
-            Quis quorum aliqua sint quem legam fore sunt eram irure aliqua
-            veniam tempor noster veniam enim culpa labore duis sunt culpa nulla
-            illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-          </p>
-        </div>
-      </div>
-      <div className="testimonial-item col-lg-3 col-md-6 d-flex align-items-stretch">
-        <div className="stars">
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-        </div>
-        <div className="profile">
-          <img
-            src="assets/clients/kyf-motos.jpg"
-            className="testimonial-img"
-            alt
-          />
-          <h3>K&F Motos</h3>
-          <p>
-            Quis quorum aliqua sint quem legam fore sunt eram irure aliqua
-            veniam tempor noster veniam enim culpa labore duis sunt culpa nulla
-            illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-          </p>
-        </div>
-      </div>
-      <div className="testimonial-item col-lg-3 col-md-6 d-flex align-items-stretch">
-        <div className="stars">
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-          <i className="fa fa-star" />
-        </div>
-        <div className="profile">
-          <img
-            src="assets/clients/cliente1.jpeg"
-            className="testimonial-img"
-            alt
-          />
-          <h3>Ingeniero Freddy</h3>
-
-          <p>
-            Quis quorum aliqua sint quem legam fore sunt eram irure aliqua
-            veniam tempor noster veniam enim culpa labore duis sunt culpa nulla
-            illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-          </p>
-        </div>
-      </div>
+      <Swiper 
+            navigation={true}
+            slidesPerView={1}
+            spaceBetween={30}
+            loop={true}
+            loopFillGroupWithBlank={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false
+            }}    
+            pagination={{
+              clickable: true
+            }}
+            className="mySwiper">
+        {ApiClients.map((item, index) => (
+        <SwiperSlide>
+          <div className="testimonial-item d-flex align-items-stretch" key={index}>
+            <div className="stars">
+              <i className="fa fa-star" />
+              <i className="fa fa-star" />
+              <i className="fa fa-star" />
+              <i className="fa fa-star" />
+              <i className="fa fa-star" />
+            </div>
+            <div className="profile">
+              <img
+                src={item.image}
+                className="testimonial-img"
+                alt
+              />
+              <h3>{item.client}</h3>
+              <h4>{item.description}</h4>
+              <br/>
+              <p>
+                {item.testimony}
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
